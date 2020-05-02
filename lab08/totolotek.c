d43529a635732cc2413dff42c0a733b6855dafc3
adamklekowski <klekowskiadam@gmail.com>
2020-05-02 15:07:03 +0200

    add totolotek.c

---
+++ lab08/totolotek.c
@@ -0,0 +1,50 @@
+/* Przy użyciu funkcji setsid stwórz demona, który będzie pracował nawet po wylogowaniu się użytkownika.
+Demon co kilkadziesiąt sekund ma losować 6 liczb z przedziału [1,49] i zapisywać je do pliku 'wyniki_lotto'.
+Po uruchomieniu demona, zamknij terminal, w którym został uruchomiony, natomiast w innym terminalu sprawdź,
+czy rzeczywiście co kilkadziesiąt sekund zmieniają się wyniki losowań. */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#include <time.h>
+
+int main(void) {
+    FILE *fp = NULL;
+    pid_t pid, sid;
+    pid = fork();
+    sid = getsid(0);
+    if (pid < 0) exit(EXIT_FAILURE);
+    if (pid > 0) {
+        printf("Rodzic:  pid=%d ppid=%d sid=%d\n",getpid(),getppid(),sid);
+        exit(EXIT_SUCCESS);
+    }
+    if (pid == 0) {
+        printf("Dziecko: pid=%d ppid=%d sid=%d\n",getpid(),getppid(),sid);
+        umask(0);
+        sid = setsid();
+
+        if (sid < 0) exit(EXIT_FAILURE);
+
+    }
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    fp = fopen ("wyniki_lotto.txt", "w+");
+    time_t tt;
+    srand(time(&tt));
+    while (1)
+    {
+        sleep(5);
+        fprintf(fp, "%d\n", (rand()%50)+1);
+        fflush(fp);
+    }
+    fclose(fp);
+    exit(EXIT_SUCCESS);
+}
